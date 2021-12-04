693ad0fbe5ef55c97329112de390864c123346e8
Neno Loje 🧀 <NenoALM@outlook.com>
2021-12-04 23:39:34 +0000

    Updated ConfigureBranchPolicies.ps1

--- Pipelines/Utils/ConfigureBranchPolicies.ps1
+++ Pipelines/Utils/ConfigureBranchPolicies.ps1
@@ -1,7 +1,7 @@
 # Docs: https://docs.microsoft.com/en-us/cli/azure/repos/policy/approver-count?view=azure-cli-latest&WT.mc_id=DOP-MVP-21138
 
+$repo = "e2e-aspnetcore6"
 $branch = 'refs/head/main'
 
 # Approver count policy
-az repos policy approver-count create --branch $branch --enabled:true --blocking:true --minimum-approver-count:1 --creator-vote-counts:yes --reset-on-source-push:true
-
+az repos policy approver-count create --repository-id $repo --branch $branch --enabled:true --blocking:true --minimum-approver-count:1 --creator-vote-counts:yes --reset-on-source-push:true
