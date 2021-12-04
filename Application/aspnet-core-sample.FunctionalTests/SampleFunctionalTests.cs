46c9ebeaf61f4eeba4aa4a32ecf8cc32304d7102
Neno Loje 🧀 <NenoALM@outlook.com>
2021-12-04 21:09:01 +0000

    Merged PR 190: [Reliability] Add job to check for test results
    Thank you for your contribution to the **e2e-aspnetcore6** repository.
    
    ## Before submitting this PR, please make sure:
    
    - [X] Your code builds clean without any errors or warnings
    - [X] You are using approved terminology
    - [X] You have added unit tests

---
+++ Application/aspnet-core-sample.FunctionalTests/SampleFunctionalTests.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace aspnet_core_sample.FunctionalTests
+{
+    [TestClass]
+    public class SampleFunctionalTests
+    {
+        private static TestContext testContext;
+        private RemoteWebDriver driver;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            SampleFunctionalTests.testContext = testContext;
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            driver = GetChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        [Timeout(600000)]
+        [TestCategory("UI")]
+        public void SampleFunctionalTest1()
+        {
+            var webAppUrl = testContext.Properties["webAppUrl"].ToString();
+            string expectedTitle = "Home page - ASP.NET Core";
+
+            var startTimestamp = DateTime.Now.Millisecond;
+            var endTimestamp = startTimestamp + 60 * 10 * 1000;
+
+            while (true)
+            {
+                try
+                {
+                    driver.Navigate().GoToUrl(webAppUrl);
+                    Assert.AreEqual(expectedTitle, driver.Title, $"Expected title to be '{expectedTitle}'");
+
+                    ((OpenQA.Selenium.ITakesScreenshot)driver).GetScreenshot().SaveAsFile(@"Screenshot.png", OpenQA.Selenium.ScreenshotImageFormat.Png);
+
+                    testContext.AddResultFile(@"Screenshot.png");
+
+                    break;
+                }
+                catch
+                {
+                    var currentTimestamp = DateTime.Now.Millisecond;
+                    if (currentTimestamp > endTimestamp)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+        private RemoteWebDriver GetChromeDriver()
+        {
+            var path = Environment.GetEnvironmentVariable("ChromeWebDriver");
+            var options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                return new ChromeDriver(path, options, TimeSpan.FromSeconds(300));
+            }
+            else
+            {
+                return new ChromeDriver(options);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,16 @@
 This is a sample project, created for training and demonstration purposes only. 
 
 For a complete list of features, see the [docs folder](Docs/Overview.md).
+
+## Environments
+
+Name     | Public URL               | Deployment history                       |
+---------|--------------------------|------------------------------------------|
+**TEST** | [e2e-app-test][env-test] | [e2e-aspnetcore6-test][env-test-history] |
+**PROD** | [e2e-app-prod][env-prod] | [e2e-aspnetcore6-prod][env-prod-history] |
+
+[env-test]:         https://e2e-app-test.azurewebsites.net
+[env-prod]:         https://e2e-app-prod.azurewebsites.net
+
+[env-test-history]: https://dev.azure.com/nenoalm/E2E/_environments/14
+[env-prod-history]: https://dev.azure.com/nenoalm/E2E/_environments/15
